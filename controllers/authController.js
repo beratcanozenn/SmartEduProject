caeecec66cf54eb468d82487e9966cebc7705f8f
Beratttt <beratozen2510@gmail.com>
2023-08-17 02:22:03 +0300

    login page and bcrypt

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,5 @@
 const User = require("../models/User");
-
+const bcrypt = require("bcrypt");
 exports.createUser = async (req, res) => {
   try {
     const user = await User.create(req.body);
@@ -7,6 +7,37 @@ exports.createUser = async (req, res) => {
       status: "Success",
       user,
     });
+  } catch (error) {
+    res.status(400).json({
+      status: "Fail",
+      error,
+    });
+  }
+};
+
+exports.loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    await User.findOne({ email }, (err, user) => {
+      if (user) {
+        bcrypt.compare(password, user.password, (err, same) => {
+          if (same) {
+            // User Session
+            res.status(200).send("You are logged in");
+          }
+        });
+      }
+    });
+
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+    if (!user) {
+      res.status(400).send("kullanici yok");
+    }
+    const same = await bcrypt.compare(password, user.password);
+    if (same) {
+      res.status(200).send("login");
+    }
   } catch (error) {
     res.status(400).json({
       status: "Fail",
--- routers/userRoute.js
+++ routers/userRoute.js
@@ -2,4 +2,5 @@ const authController = require("../controllers/authController");
 const router = require("express").Router();
 
 router.route("/signup").post(authController.createUser);
+router.route("/login").post(authController.loginUser);
 module.exports = router;
