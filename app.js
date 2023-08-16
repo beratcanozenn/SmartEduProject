81e64429cc65ccf9dcc0835dda380b0434f57b56
Beratttt <beratozen2510@gmail.com>
2023-08-16 16:34:11 +0300

    setting route page

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-
+const pageRoute = require("./routers/pageRoute");
 //Template Engine
 app.set("view engine", "ejs");
 
@@ -8,13 +8,7 @@ app.set("view engine", "ejs");
 app.use(express.static("public"));
 
 // Routes
-app.get("/", (req, res) => {
-  res.status(200).render("index", { page_name: "index" });
-});
-
-app.get("/about", (req, res) => {
-  res.status(200).render("about", { page_name: "about" });
-});
+app.use("/", pageRoute);
 
 const port = 3000;
 app.listen(port, () => {
