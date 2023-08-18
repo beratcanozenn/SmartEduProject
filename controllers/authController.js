d974b2e75203e458a9e4a461ead1a57c6a3c3ec6
Beratttt <beratozen2510@gmail.com>
2023-08-19 00:17:42 +0300

    Add express-validator for register,login

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,16 +2,20 @@ const User = require("../models/User");
 const Category = require("../models/Category");
 const Course = require("../models/Course");
 const bcrypt = require("bcrypt");
+const { validationResult } = require("express-validator");
 exports.createUser = async (req, res) => {
   try {
     const user = await User.create(req.body);
-    console.log("Berat");
+    res.flash("success", "Başarıyla kayıt oldunuz");
     res.status(201).redirect("/login");
   } catch (error) {
-    res.status(400).json({
-      status: "Fail",
-      error,
-    });
+    const errors = validationResult(req);
+
+    for (let i = 0; i < errors.array().length; i++) {
+      req.flash("error", `${errors.array()[i].msg}`);
+    }
+
+    res.status(400).redirect("/register");
   }
 };
 
@@ -25,16 +29,12 @@ exports.loginUser = async (req, res) => {
         req.session.userID = user._id;
         res.status(200).redirect("/users/dashboard");
       } else {
-        res.status(401).json({
-          status: "false",
-          error: "İnvalid Password",
-        });
+        req.flash("error", "Your password is not correct");
+        res.status(400).redirect("/login");
       }
     } else {
-      res.status(404).json({
-        status: "False",
-        error: "User Not Found",
-      });
+      req.flash("error", "User is not exist !");
+      res.status(400).redirect("/login");
     }
   } catch (error) {
     res.status(500).json({ status: "error", error: error.message });
@@ -48,11 +48,12 @@ exports.logoutUser = (req, res) => {
 };
 
 exports.getDashboardPage = async (req, res) => {
-  const user = await User.findOne({ _id: req.session.userID }).populate('courses');
+  const user = await User.findOne({ _id: req.session.userID }).populate(
+    "courses"
+  );
   const categories = await Category.find({});
   const courses = await Course.find({ user: req.session.userID });
   res
     .status(200)
     .render("dashboard", { page_name: "dashboard", user, categories, courses });
 };
-
--- routers/userRoute.js
+++ routers/userRoute.js
@@ -1,7 +1,25 @@
 const authController = require("../controllers/authController");
+const User = require("../models/User");
 const router = require("express").Router();
 const authMiddleware = require("../middlewares/authMiddleware");
-router.route("/signup").post(authController.createUser);
+const { body } = require("express-validator");
+router.route("/signup").post(
+  [
+    body("name").not().isEmpty().withMessage("Please Enter Your Name"),
+    body("email")
+      .isEmail()
+      .withMessage("Please Enter Valid email")
+      .custom((userEmail) => {
+        return User.findOne({ email: userEmail }).then((user) => {
+          if (user) {
+            return Promise.reject("Email is already exist!");
+          }
+        });
+      }),
+    body("password").not().isEmpty().withMessage("Please Enter Yor Password"),
+  ],
+  authController.createUser
+);
 router.route("/login").post(authController.loginUser);
 router.route("/logout").get(authController.logoutUser);
 router.route("/dashboard").get(authMiddleware, authController.getDashboardPage);
